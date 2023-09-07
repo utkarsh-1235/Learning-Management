ae484154efa620bf86c829f2d8a82cf26ab3129c
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-09-07 22:30:34 +0530

    midleware replaced to auth middleware and error.middleware is added

---
+++ backend/Middleware/auth.middleware.js
@@ -0,0 +1,69 @@
+import AppError from "../utils/error.util.js";
+import jwt from 'jsonwebtoken';
+
+const isLoggedIn = async (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return next(new AppError('Unauthenticated, please login again', 401));
+    }
+
+    const userDetails = await jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = userDetails;
+
+    next();
+}
+
+const authorizedRoles = (...roles) => async (req, res, next) => {
+    const currentUserRole = req.user.role;
+    if (!roles.includes(currentUserRole)) {
+        return next(
+            new AppError('You do not have permission to access this route', 403)
+        )
+    }
+    next();
+}
+
+const authorizeSubscriber = async(req, res, next) => {
+    const subsciption = req.user.subsciption;
+    const currentUserRole = req.user.role;
+    if (currentUserRole !== 'ADMIN' && subsciption.status !== 'active') {
+        return next(
+            new AppError('Please subscribce to access this route!', 403)
+        )
+    }
+
+    next();
+}
+
+export {
+    isLoggedIn,
+    authorizedRoles,
+    authorizeSubscriber
+}
+
+// const JWT = require("jsonwebtoken");
+// //require('dotenv').config();
+
+// // router level middleware function
+// const jwtAuth = (req, res, next) => {
+//   // get cookie token(jwt token generated using json.sign()) form the request
+//   const token = (req.cookies && req.cookies.token) || null;
+
+//   // return response if there is no token(jwt token attached with cookie)
+//   if (!token) {
+//     return res.status(400).json({ success: false, message: "NOT authorized" });
+//   }
+
+//   // verify the token
+//   try {
+//     const payload = JWT.verify(token, process.env.SECRET);
+//     req.user = { id: payload.id, email: payload.email };
+//   } catch (error) {
+//     return res.status(400).json({ success: false, message: error.message });
+//   }
+//   next();
+// };
+
+// module.exports = jwtAuth;
